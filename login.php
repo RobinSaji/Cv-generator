92b808dbaf3c9cbface8eb2d8fa7e4d3d5f0ccbc
RobinSaji <85700150+RobinSaji@users.noreply.github.com>
2022-03-29 11:55:18 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,35 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=`, initial-scale=1.0">
+    <title>login page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <div class="container">
+        <p class="login_text">login in with mail</p>
+        <form class="firstform">  
+            
+            <input type ="text" class="email" name="name" placeholder="username">
+            <br>
+            <input type="password" class="passwd" name="passwd" placeholder="enter the password">
+            <br>
+            <button class="btn" >login</button>
+            <br>
+            <br>
+            <h3>doest have an account?<a href="register.php">Register here.</a><h3>  
+            
+    </div>
+    
+    
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,35 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>registration page</title>
+    <link rel="stylesheet" href="style1.css">
+
+</head>
+<body>
+    <div class="container">
+        <h2 class="login_text">Sign Up Now</h2>
+        <form class="Secondform">  
+           
+            <input type ="text" class="username" name="name" placeholder="username">
+    
+            <!-- <input type="email" class="mail" name="mail" placeholder="email"> -->
+            <br>
+            <input type="password" class="password" name="password" placeholder="password">
+            <br>
+            <input type="password" class="passwd2" name="passwd2" placeholder="confirm password">
+            <br>
+            <button class="btn" >register</button>
+            <br>
+            <br>
+            <h3>already have an account?<a href="login.php">login here.</a><h3>  
+            
+        </form>
+    </div>
+    
+</body>
+</html>
